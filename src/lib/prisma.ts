16c9506e9ef34843824a27fab7c265bf34ffdeac
Fernando González Tostado <68973307+esponges@users.noreply.github.com>
2025-06-11 16:55:09 -0600

    integrate prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,9 +1,7 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "../generated/client";
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
-};
+const prisma = new PrismaClient({
+  datasourceUrl: import.meta.env.DATABASE_URL,
+})
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient();
-
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma; 
+export default prisma;
